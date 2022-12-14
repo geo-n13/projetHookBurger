dea285f54a247897228b8d25dcca7e98d3ee3e6d
geo-n13 <80384562+geo-n13@users.noreply.github.com>
2022-12-14 23:00:32 +0100

    Premier commit d'une classe "test"

--- src/burger/test/ClientTest.java
+++ src/burger/test/ClientTest.java
@@ -1,4 +1,17 @@
 package burger.test;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 public class ClientTest {
+    @Test
+    public void testCommanderBurger(){
+        // TODO: Test de "commanderBurger()" à faire
+    }
+
+    @Test
+    public void testMangerBurger(){
+        // TODO: Test de "mangerBurger()" à faire
+    }
 }
