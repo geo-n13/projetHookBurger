4be1145fe80e2b2970ef2edad705a53c33d63f12
geo-n13 <80384562+geo-n13@users.noreply.github.com>
2022-12-15 22:20:08 +0100

    Instructions pour la réalisation du Main.java

--- src/burger/modele/Main.java
+++ src/burger/modele/Main.java
@@ -2,7 +2,13 @@ package burger.modele;
 
 public class Main {
     public static void main(String[] args) {
-       
+       //TODO : Générer le Main.java à travers les choses suivantes :
+        // Créer un restaurant
+        // Créer des clients
+        // Générer plusieurs tables
+        // Générer des commandes
+        // Calculer la note de chaque table
+        // Ajouter ça au chiffre d'affaire de la journée du restaurant
     	
     	
     }
