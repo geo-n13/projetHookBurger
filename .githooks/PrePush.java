d5fb35aa88a5ac675c6fab453fca4516bdddd91f
BigBern44 <you@example.com>
2022-12-16 23:06:00 +0100

    test prepush hook

--- .githooks/PrePush.java
+++ .githooks/PrePush.java
@@ -1,5 +1,19 @@
 public class PreCommit {
-    public static void main (String[] args) {
-      System.out.println("test");
+    public static void main (String[] args) throws IOException{
+
+        // Args[0] == local
+        // Args[1] == remote
+
+        System.out.println("Local :"+args[0]);
+        System.out.println("Remote :"+args[1]);
+
+        boolean res = (args[1].contains("yvpierre"));
+        System.out.println("Bonjour, 'yvpierre' dans le remote, push accepté");
+        System.out.println(res);
+        if(res){
+            System.exit(0);
+        }else {
+            System.exit(1);
+        }
     }
   }
